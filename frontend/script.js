b33f18e457a544d53c3c2f5bc2fbc392216ea461
falconzero7 <rfrodriguesap@gmail.com>
2025-04-22 22:30:45 -0300

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,8 +0,0 @@
-
-function entrar() {
-  alert('Login simulado. Integração futura com sistema de autenticação.');
-}
-
-function solicitar() {
-  alert('Requisição de acesso registrada. Em breve entraremos em contato.');
-}
